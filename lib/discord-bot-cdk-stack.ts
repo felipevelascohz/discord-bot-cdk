2743a7ffd383cc008317b721217fb387759c8fb6
JP <juanpzulu1@gmail.com>
2024-05-16 09:19:27 -0500

    PruebaCF

--- lib/discord-bot-cdk-stack.ts
+++ lib/discord-bot-cdk-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { aws_cloudfront } from 'aws-cdk-lib/aws-cloudfront';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DiscordBotCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const imagesBucket = new Bucket(this, 'S3Bucket')
+    new aws_cloudfront(this, 'GifsCloudFront', {
+      originConfigs: [
+        {
+          behaviors: [
+            {
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DiscordBotCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });_______________________________
+            }
+          ],
+          s3OriginSource: {
+            s3BucketSource: imagesBucket } 
+          }
+        ]
+    }
+    )
   }
 }
