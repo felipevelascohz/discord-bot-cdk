503a2fcf47422bf437efbe5b246a1dbeb27a11e5
JP <juanpzulu1@gmail.com>
2024-05-25 12:38:52 -0500

    AutoScalingBot

--- lib/discord-bot-cdk-stack.ts
+++ lib/discord-bot-cdk-stack.ts
@@ -9,7 +9,7 @@ import { EcsApplication } from 'aws-cdk-lib/aws-codedeploy';
 import { aws_ec2 } from 'aws-cdk-lib';
 import { Tags } from 'aws-cdk-lib';
 import { aws_ecs_patterns } from 'aws-cdk-lib';
-import { ApplicationScalingAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
+import { ApplicationScalingAction, Ec2Action } from 'aws-cdk-lib/aws-cloudwatch-actions';
 import { aws_scheduler } from 'aws-cdk-lib';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -27,7 +27,13 @@ export class DiscordBotCdkStack extends cdk.Stack {
 
     const vpc = new aws_ec2.Vpc ( this, 'vpc', {maxAzs: 1});
     const cluster = new aws_ecs.Cluster ( this, 'EcsCluster', {vpc});
-    
+      
+cluster.addCapacity('DefaultAutoScalingGroupCapacity', {
+  instanceType: new aws_ec2.InstanceType("t2.micro"),
+  desiredCapacity: 1,
+  maxCapacity: 1,
+});
+
     const taskDefinition = new aws_ecs.Ec2TaskDefinition ( this, 'TaskDef');
     taskDefinition.addContainer('TheContainer', {
       image: aws_ecs.ContainerImage.fromRegistry('zuluaga1996/botdc:latest'),
@@ -46,8 +52,7 @@ export class DiscordBotCdkStack extends cdk.Stack {
       /* schedule: cdk.aws_applicationautoscaling.Schedule.expression('rate ( 1 minute)'), */
       propagateTags: aws_ecs.PropagatedTagSource.TASK_DEFINITION,
 
-      public addAsgCapacityProvider(provider: AsgCapacityProvider, options?: AddAutoScalingGroupCapacityOptions): void
-
+      
     });
   }
 }
